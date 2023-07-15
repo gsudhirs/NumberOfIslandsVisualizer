ba90dc71084fcff4d5d356c8a26938e51adf3248
Gahan Sudhir <111015445+gsudhirs@users.noreply.github.com>
2023-07-15 15:40:40 -0500

    Added app.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NumIsland from './numisland/numisland'
+
+function App() {
+  return (
+    <div>
+      <h1>Number of Islands Visualizer</h1>
+      <NumIsland />
+    </div>
+  );
+};
+
+export default App;
